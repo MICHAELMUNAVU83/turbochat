e0fd749723e43821181eac7e37f6eeb47e3f9a2c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-25 13:47:01 +0300

    Displayed shows and users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
      get '/users/sign_out' => 'devise/sessions#destroy' 
   end
+
+  get "user/:id", to: "users#show", as: "user"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding database..."
+
+rooms = Room.create([
+    { name: "General", is_private: false },
+    { name: "Random", is_private: false },
+    { name: "Introductions", is_private: false },
+    { name: "Help", is_private: false },
+    { name: "Jobs", is_private: false },
+
+    { name: "Ruby", is_private: false },
+    { name: "Rails", is_private: false },
+
+
+])
+
+puts "Seeding complete!"
+
+
+users = User.create([
+
+    {email:"john@gmail.com", password:"1234556", password_confirmation:"1234556"},
+    {email:"kim@gmail.com", password:"1234556", password_confirmation:"1234556"},
+    {email:"jama@gmail.com", password:"1234556", password_confirmation:"1234556"}
+
+])
